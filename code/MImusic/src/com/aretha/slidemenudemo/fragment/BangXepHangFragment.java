a3654b0ff4bbcd41a2ead34addbc15f3461d031b
hoangtanquan267@gmail.com <hoangtanquan267@gmail.com@319787fd-3b21-e5cc-9f87-a16ac6c5dcde>
2014-12-28 23:46:47 +0000

    Add Roboto fonts Change and add empty message Restyle empty message Add font for message Change flow get and set header no data

--- code/MImusic/src/com/aretha/slidemenudemo/fragment/BangXepHangFragment.java
+++ code/MImusic/src/com/aretha/slidemenudemo/fragment/BangXepHangFragment.java
@@ -5,6 +5,8 @@ import vnp.com.mimusic.activity.RootMenuActivity;
 import vnp.com.mimusic.adapter.BangXepHangAdaper;
 import vnp.com.mimusic.view.BangXepHangHeaderView;
 import vnp.com.mimusic.view.BangXepHangHeaderView.BangXepHangHeaderInterface;
+import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class BangXepHangFragment extends Fragment implements OnItemClickListener, View.OnClickListener {
 	@Override
@@ -22,12 +25,12 @@ public class BangXepHangFragment extends Fragment implements OnItemClickListener
 
 	private ListView bangxephang_list;
 	private View header_nodata;
+	private String[] items;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.bangxephang, null);
 		bangxephang_list = (ListView) view.findViewById(R.id.bangxephang_list);
-		header_nodata = inflater.inflate(R.layout.bangxephang_nodata, null);
 		BangXepHangHeaderView bangxephang_bangxephangheader = (BangXepHangHeaderView) view.findViewById(R.id.bangxephang_bangxephangheader);
 		bangxephang_bangxephangheader.setBangXepHangHeaderInterface(new BangXepHangHeaderInterface() {
 
@@ -40,21 +43,39 @@ public class BangXepHangFragment extends Fragment implements OnItemClickListener
 		callData(true);
 		return view;
 	}
-
+	
 	private void callData(boolean b) {
+		//remove header no data when check or not check so luong radio
 		try {
 			bangxephang_list.removeHeaderView(header_nodata);
-		} catch (Exception exception) {
-
+		} catch (Exception exception) {}
+		//get data for list
+		if (b) {
+			items = new String[0];
+		} else {
+			items = new String[] { "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a" };
 		}
-		BangXepHangAdaper adaper = new BangXepHangAdaper(getActivity(), new String[] { "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a" });
-		if (!b) {
-			bangxephang_list.setAdapter(null);
+		//header no data
+		LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+		header_nodata = inflater.inflate(R.layout.bangxephang_nodata, null);
+		//	set custom font for message
+		TextView noDataMessage = (TextView) header_nodata.findViewById(R.id.no_data_message);
+		if (b) {
+			noDataMessage.setText(getResources().getString(R.string.bangxephang_so_luong_no_data));
+		} else {
+			noDataMessage.setText(getResources().getString(R.string.bangxephang_doanh_thu_no_data));
+		}
+		Typeface typeface = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Regular.ttf");
+		noDataMessage.setTypeface(typeface);
+		BangXepHangAdaper adaper = new BangXepHangAdaper(getActivity(), items);
+		//set data for listview
+		if (items.length > 0) {
+			bangxephang_list.setOnItemClickListener(this);
+			bangxephang_list.setAdapter(adaper);
+		} else {
 			bangxephang_list.addHeaderView(header_nodata);
-			adaper = new BangXepHangAdaper(getActivity(), new String[] {});
+			bangxephang_list.setAdapter(adaper);
 		}
-		bangxephang_list.setOnItemClickListener(this);
-		bangxephang_list.setAdapter(adaper);
 	}
 
 	@Override
