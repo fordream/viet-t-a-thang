12fe8ade41a43b0b242ef4bb76b87ee59b4abc9d
vnp.mr.truong@gmail.com <vnp.mr.truong@gmail.com@319787fd-3b21-e5cc-9f87-a16ac6c5dcde>
2015-03-13 08:20:54 +0000

    aa

--- code/MImusic/src/vnp/com/mimusic/util/Conts.java
+++ code/MImusic/src/vnp/com/mimusic/util/Conts.java
@@ -179,7 +179,6 @@ public class Conts {
 				String errorCode = "";
 				RestClient restClient = (RestClient) object;
 				String response = restClient.getResponse();
-
 				try {
 
 					// if (API.API_R015.equals(api)) {
